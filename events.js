a1e80c250a7b8e6a1a856ff4777262ae99bcd786
Marcos Moreira <marcos.ph@hotmail.es>
2023-08-29 21:22:17 -0300

    solucion al problema del click

--- events.js
+++ events.js
@@ -2,5 +2,5 @@ function saludo(){
     alert("Hola! Soy el Div :)")
 }
 
-const btn = document.querySelector(".boton");
+const btn = document.querySelector(".boton-saludo");
 btn.addEventListener("click", saludo);
